d483681c376f0e2aac425e38d2e0769abc1e8fc7
Nazanin Goudarzi <31389568+nazaninGoudarzi@users.noreply.github.com>
2023-08-13 01:04:18 +0330

    Add files via upload

---
+++ DAO/DAO.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract DAO {
+    mapping (address => uint) public userBalances;
+
+    function addToBalance() public payable {
+        userBalances[msg.sender] += msg.value * (10**18);
+    }
+
+    function getUserBalance(address user) public view returns(uint) {
+        return userBalances[user];
+    }
+
+    function withdrawBalance(uint amount) public {
+        if(userBalances[msg.sender] >= amount){
+            (bool temp,) = msg.sender.call{value : (amount / (10**18))}("");
+            require(temp, 'error');
+            userBalances[msg.sender] -= amount;
+        }
+    }
+}
